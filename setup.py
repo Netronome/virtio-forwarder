e668b32f717a6c1aece065467ba7cecbdf83beb1
Frik Botha <frik.botha@netronome.com>
2018-01-11 17:00:26 +0200

    Include setup.py for rtd versioning
    Readthedocs does not allow the version fields to be filled when importing via the
    github webhook - there is no make hook as in copr; it simply finds and copies the
    relevant doc files. It does, however, allow the invocation of a setup.py script,
    which we exploit here to populate the version fields. Note that this is somewhat
    of a hack, and it should be revised if rtd decides to introduce relevant
    infrastructure in the future.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# NOTE: This is a hack which duplicates the functionality of the prepare_docs
+# make target.
+# This file does not follow the goals and principles of your typical setup.py.
+# It is used here for a single purpose, and that is to populate the version
+# fields in doc/conf.py when readthedocs' builder builds the documentation. This
+# file would not exist had rtd implemented a make target hook in its code import
+# infrastructure. The only agile hook they do include at this stage is the
+# invocation of setup.py; hence this file.
+
+import subprocess
+import re
+
+cmd = ['git', 'describe', '--tags', '--long']
+output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+
+v_maj = output.split('.')[0]
+v_min = output.split('.')[1]
+v_patch = output.split('.')[2].split('-')[0]
+v_build = output.split('-')[1]
+tag_string = "%s.%s.%s" % (v_maj, v_min, v_patch)
+
+with open ('./doc/conf.py', 'r') as f:
+    lines = f.readlines()
+
+lines_new = []
+for line in lines:
+    tmp = line
+    tmp = re.sub('__VRELAY_TAG_VERSION__', tag_string, tmp)
+    tmp = re.sub('__VRELAY_BUILD_VERSION__', v_build, tmp)
+    tmp = re.sub('__APP__NAME__', 'virtio-forwarder', tmp)
+    lines_new.append(tmp)
+
+with open ('./doc/conf.py', 'w') as f:
+    for line in lines_new:
+        f.write(line)
