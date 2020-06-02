Netronome/virtio-forwarder

commit 309542ddded0aa6b1fc2d01cdeb41e5e6754129e

Conditionally add rhel specific CFLAGS
We only want to include a few rhel specific CFLAGS when we are building
on a rhel based install. This commit adds the needed conditional logic
in the makefile.

Change-Id: Ie09b6ba0352ddd3ceb1280a240e35001f6942e9c
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>

Makefile
@@ -32,6 +32,7 @@
 ARCH ?= x86_64
 RTE_SDK ?= /usr/share/dpdk
 RTE_TARGET ?= $(ARCH)-default-linuxapp-gcc
+DISTRO := $(shell awk '/^ID=/' /etc/*-release | sed 's/[^=]*=//' | tr -d '"')
 
 ifeq ($(wildcard $(RTE_SDK)),)
     $(warning RTE_SDK=$(RTE_SDK) does not exist!)
@@ -92,7 +93,11 @@ CFLAGS += -D_GNU_SOURCE
 #CFLAGS += -DVIRTIO_ECHO -Wno-error=unused-but-set-variable -Wno-error=unused-parameter -Wno-error=unused-function
 CFLAGS += $(WERROR_FLAGS)
 CFLAGS += -Ipb2
-CFLAGS += -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld
+
+# Only add redhat spesific CFLAGS when dealing with a rhel based install
+ifeq ($(DISTRO),$(filter $(DISTRO), rhel centos fedora))
+	CFLAGS += -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld
+endif
 
 LDLIBS += -lprotobuf-c -lzmq
 

commit 0f914e8338ff36f87666570468108e52ceaea33b

Conditionally add shared library linker flags
When creating virtio-forwarder builds that make use of statically built
DPDK libraries, don't include linker flags that pertain to linking
against shared libraries. For example "-Wl,-z,now" turns off lazy
binding but this is only relevant when linking against shared objects

Fixes: 309542d ("Conditionally add rhel specific CFLAGS")
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>
Change-Id: Icc01926b097b41fa42082b392753f7fdd3b7b557

Makefile
@@ -94,9 +94,12 @@ CFLAGS += -D_GNU_SOURCE
 CFLAGS += $(WERROR_FLAGS)
 CFLAGS += -Ipb2
 
-# Only add redhat spesific CFLAGS when dealing with a rhel based install
+# Only add redhat specific CFLAGS when dealing with a rhel based install
+# These flags are only relevant when linking to a shared DPDK library
+ifeq ($(CONFIG_RTE_BUILD_SHARED_LIB),y)
 ifeq ($(DISTRO),$(filter $(DISTRO), rhel centos fedora))
 	CFLAGS += -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld
+endif
 endif
 
 LDLIBS += -lprotobuf-c -lzmq