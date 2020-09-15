Netronome/virtio-forwarder

commit 239867ce75d55ca9ac8052d2c0f3c7038ef5f2a0

Add vhost-user socket name and path to stats query
For debugging purposes it is useful to map a given relay to the
vhost-user socket it is currently servicing. This patch adds a
vhost-user socket field to the stats struct and the required zmq
infrastructure for this purpose. The virtioforwarder_stats.py script
will print this new field for relays that are currently servicing a
vhost-user socket. For relays that aren't associated with a vhost-user
socket yet the script will report "NONE"

Change-Id: I0a42303a291026479ccdd26c390ea676f2993e62
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>
Reviewed-by: Louis Peens <louis.peens@netronome.com>

virtio_vhostuser.c
@@ -100,7 +100,6 @@ static char vhost_socket_name_prefix[128];
 static char vhost_socket_name_suffix[128];
 static bool mk_default_sockets;
 struct virtio_vhostuser_conf g_vio_worker_conf;
-static char *relay_ifname_map[MAX_RELAYS];
 
 static const char
 *virtio_vhostuser_id_to_name(unsigned id, char *name, size_t namesz)

virtio_worker.h
@@ -50,6 +50,9 @@ typedef uint16_t dpdk_port_t;
 typedef uint8_t dpdk_port_t;
 #endif
 
+/* Shared array of vhost_user socket names, indexed by virtio-relay ID */
+char *relay_ifname_map[MAX_RELAYS];
+
 extern struct virtio_vhostuser_conf g_vio_worker_conf;
 
 int virtio_forwarders_initialize(void);
@@ -148,6 +151,7 @@ struct virtio_worker_stats
 	bool virtio2vf_active;
 
 	/* The following fields are only valid when virtio2vf_active is true. */
+	char vhost_socket_name[128];
 	int virtio2vf_cpu;
 	uint64_t virtio_rx;
 	uint64_t virtio_rx_bytes;

scripts/virtioforwarder_stats.py
@@ -114,6 +114,9 @@ def _output_flat(reply, suppress_zero):
         middle = ['vf']
         out(r.vf, 'pci_addr_str')
 
+        middle = ['vhost']
+        out(r.vhost, 'vhost_socket_name')
+
         cpu_fieldset = fieldset(r.cpu)
 
         middle = ['vf_to_vm']

virtio_worker.c
@@ -2190,6 +2190,15 @@ virtio_forwarder_get_stats(unsigned virtio_id, struct virtio_worker_stats *stats
 	vio_state_t virtio_state = r->vio.state;
 	virtio_state_to_str(virtio_state, stats->virtio_internal_state,
 			sizeof(stats->virtio_internal_state));
+
+	/* If a relay does not have a vhost-user socket accociated with it
+         * report NONE in stats output */
+	if (relay_ifname_map[virtio_id])
+		strncpy(stats->vhost_socket_name, relay_ifname_map[virtio_id],
+			sizeof(stats->vhost_socket_name));
+	else
+		strcpy(stats->vhost_socket_name, "NONE");
+
 	if (virtio_state == VIRTIO_READY) {
 		stats->virtio2vf_active = true;
 		stats->virtio2vf_cpu = r->vio.vio2vf_cpu;

virtioforwarder.proto
@@ -113,6 +113,15 @@ message RelayState {
     // CPU pair assigned to this relay.
     optional CPU cpu = 4;
 
+    // A structure to describe the vhost socket name associated with this relay
+    message VHOST {
+        // vhost socket name associated with this relay
+        required string vhost_socket_name = 1;
+    }
+
+    // VHOST interface associated with this relay
+    required VHOST vhost = 5;
+
     // A structure to describe the VF, if any, attached to this relay.
     message VF {
         // PCI address of VF attached to this relay.
@@ -120,7 +129,7 @@ message RelayState {
     }
 
     // VF attached to this relay.
-    optional VF vf = 5;
+    optional VF vf = 6;
 
     // A structure to represent statistics for the VF-to-VM side of the relay
     // (the "up" direction).
@@ -171,7 +180,7 @@ message RelayState {
     }
 
     // Statistics for the VF-to-VM side of the relay (the "up" direction).
-    required VFtoVM vf_to_vm = 6;
+    required VFtoVM vf_to_vm = 7;
 
     // A structure to represent statistics for the VM-to-VF side of the relay
     // (the "down" direction).
@@ -223,10 +232,10 @@ message RelayState {
     }
 
     // Statistics for the VM-to-VF side of the relay (the "down" direction).
-    required VMtoVF vm_to_vf = 7;
+    required VMtoVF vm_to_vf = 8;
 
     // NUMA node where the relay's memory pool is allocated
-    required uint32 socket_id = 8;
+    required uint32 socket_id = 9;
 }
 
 // Request for statistics.

zmq_stats.c
@@ -63,6 +63,7 @@ struct stats_response_buffer
 	Virtioforwarder__RelayState relay_state[MAX_RELAYS];
 	Virtioforwarder__RelayState__CPU cpu[MAX_RELAYS];
 	Virtioforwarder__RelayState__VF vf[MAX_RELAYS];
+	Virtioforwarder__RelayState__VHOST vhost[MAX_RELAYS];
 	Virtioforwarder__RelayState__VFtoVM vf_to_vm[MAX_RELAYS];
 	Virtioforwarder__RelayState__VMtoVF vm_to_vf[MAX_RELAYS];
 
@@ -107,9 +108,13 @@ relay_query(
 
 	Virtioforwarder__RelayState__VMtoVF *vm_to_vf = b->vm_to_vf + j;
 	virtioforwarder__relay_state__vmto_vf__init(vm_to_vf);
+	Virtioforwarder__RelayState__VHOST *vhost = b->vhost + j;
+	virtioforwarder__relay_state__vhost__init(vhost);
 	relay_state->vm_to_vf = vm_to_vf;
 	vm_to_vf->active = s->virtio2vf_active;
 	vm_to_vf->internal_state = s->virtio_internal_state;
+	vhost->vhost_socket_name = s->vhost_socket_name;
+	relay_state->vhost = vhost;
 	if (vm_to_vf->active) {
 		cpu->has_vm_to_vf = true;
 		cpu->vm_to_vf = s->virtio2vf_cpu;

commit e84246c093d9d5e3b1e6a301127a7378eba00fd6

Declare relay_ifname_map[] as external
The relay_ifname_map array currently used to store vhost socket names
should be declared as an external variable in virtio_worker.h since the
array is used in more than one source file.

Fixes: 239867c ("Add vhost-user socket name and path to stats query")
Change-Id: Iaf1fb91f34d1dd31dc3b145212584c4731eaec53
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>

virtio_vhostuser.c
@@ -100,6 +100,7 @@ static char vhost_socket_name_prefix[128];
 static char vhost_socket_name_suffix[128];
 static bool mk_default_sockets;
 struct virtio_vhostuser_conf g_vio_worker_conf;
+char *relay_ifname_map[MAX_RELAYS];
 
 static const char
 *virtio_vhostuser_id_to_name(unsigned id, char *name, size_t namesz)

virtio_worker.h
@@ -51,7 +51,7 @@ typedef uint8_t dpdk_port_t;
 #endif
 
 /* Shared array of vhost_user socket names, indexed by virtio-relay ID */
-char *relay_ifname_map[MAX_RELAYS];
+extern char *relay_ifname_map[MAX_RELAYS];
 
 extern struct virtio_vhostuser_conf g_vio_worker_conf;
 