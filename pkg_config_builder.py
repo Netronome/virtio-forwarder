5ec5d88efa0b95da0d89c12aae6dbb4ce56624ed
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2020-09-15 15:34:16 +0200

    Expand meson support to include packaging logic
    The use of make is being deprecated in DPDK 20.11 and going
    forward virtio-forwarder will also move towards using meson and ninja
    to build the project. This commit adds the necessary meson
    infrastructure to facilitate building rpm and debian packages. The
    actual building logic is still hosted in the virtio-forwarder-dist repo.
    
    The meson.build file will to a large extent mirror the current logic in
    the Makefile
    
    Change-Id: I4a18b8ad5f75bd5e4810a3ead88a192f83a8b9a9
    Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
    Reviewed-by: Louis Peens <louis.peens@netronome.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

---
+++ pkg_config_builder.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import re
+from jinja2 import Template
+import subprocess
+
+if os.path.exists("/usr/lib64"):
+    # Rhel/CentOS/Fedora
+    library_path = "/usr/lib64"
+    pkg_config_path = os.path.join(library_path, "pkgconfig")
+    dpdk_version = subprocess.check_output(
+        ['rpm', '-q', '--qf', '%{VERSION}', 'dpdk']).decode('UTF-8')
+else:
+    # Ubuntu/Debian
+    library_path = "/usr/lib"
+    pkg_config_path = os.path.join(library_path,
+        "x86_64-linux-gnu/pkgconfig/pkgconfig")
+    dpkg_query_out = subprocess.check_output(
+        ['dpkg-query', '-f', '${Version}', '-W', 'dpdk']).decode('UTF-8')
+    dpdk_version = dpkg_query_outplit('-')[0]
+
+dpdk_libraries = []
+
+# Order is important here
+dpdk_libraries.append('-Wl,--whole-archive -L${libdir}')
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_common.*\.so$', lib)]
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_bus.*\.so$', lib)]
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_mempool.*\.so$', lib)]
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_raw.*\.so$', lib)]
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_pmd.*\.so$', lib)]
+
+dpdk_libraries.append('-Wl,--no-whole-archive')
+
+dpdk_libraries += [os.path.splitext(lib)[0] for lib in os.listdir(library_path)
+        if re.search(r'librte_(?!common|bus|mempool|raw|pmd).*\.so$', lib)]
+
+dpdk_libraries = [x.replace('librte', '-lrte') for x in dpdk_libraries]
+
+template = """prefix={{ prefix }}
+libdir=${prefix}/{{ os_lib_path }}
+includedir=${prefix}/include/dpdk
+
+Name: {{ name }}
+Description: {{ description }}
+Version: {{ version }}
+Libs: {{ libraries }}
+Cflags: -I${includedir} -include rte_config.h -march=corei7"""
+
+config_data = {
+        "prefix": "/usr",
+        "os_lib_path": os.path.split(library_path)[1],
+        "name": "DPDK",
+        "description" : "Manually built libdpdk.pc file",
+        "version" : dpdk_version,
+        "libraries": " ".join(dpdk_libraries)
+        }
+
+j2_template = Template(template)
+
+if not os.path.exists(os.path.join(pkg_config_path, "libdpdk.pc"):
+    with open(os.path.join(pkg_config_path, "libdpdk.pc"), "w") as f:
+        f.write(j2_template.render(config_data))
