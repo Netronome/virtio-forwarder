2e471eaaf59ef7d3062347457448b6ed366c2481
Chaoyong He <chaoyong.he@corigine.com>
2021-03-15 18:01:58 +0800

    CI: Add Github Action to compile DPDK 18.11 and virtio-forwarder
    This commit adds a basic Github workflow to compile virtio-forwarder on
    various distributions. The DPDK 18.11 branch is targeted from the
    dpdk-stable repository.
    
    Distributions targeted:
    * Ubuntu Bionic/Focal
    * CentOS 7/8
    * Fedora 30/31/32/33
    
    The following Meson flags are used when compiling DPDK:
    -Denable_kmods=false -Ddefault_library=shared -Dbuildtype=release
    
    A few dependencies are need to compile virtio-forwarder depending on the
    distribution
    
    Debian/Ubuntu
    * protobuf-compiler   for 'protoc'
    * protobuf-c-compiler for 'protoc-c'
    * libprotobuf-c-dev   for 'libprotobuf-c.so'
    * libzmq3-dev         for 'libzmq'
    
    RHEL/Fedora/CentOS
    * protobuf-compiler   for 'protoc'
    * protobuf-c-compiler for 'protoc-c'
    * protobuf-c-devel    for 'libprotobuf-c.so'
    * czmq-devel          for 'libzmq'
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

---
+++ .github/.ci/linux-build.sh
@@ -0,0 +1,50 @@
+function install_package {
+    if [ "$SUB_OS" = "none" ]; then
+        sudo apt-get install -q -y python3 python3-pip python3-setuptools python3-wheel \
+            protobuf-compiler protobuf-c-compiler libprotobuf-c-dev libnuma-dev libzmq3-dev ninja-build
+    elif [ "$SUB_OS" = "centos:7" ]; then
+        yum install -q -y epel-release sudo git gcc
+        sudo yum install -q -y python3 python3-pip python3-setuptools python3-wheel \
+            protobuf-compiler protobuf-c-compiler protobuf-c-devel numactl-devel czmq-devel ninja-build
+    elif [ "$SUB_OS" = "centos:8" ]; then
+        dnf install -q -y epel-release sudo git gcc
+        sudo dnf --enablerepo=powertools install -q -y protobuf-compiler protobuf-c-compiler protobuf-c-devel ninja-build
+        sudo dnf install -q -y python3 python3-pip python3-setuptools python3-wheel numactl-devel czmq-devel
+    elif [[ "$SUB_OS" = "fedora"* ]]; then
+        dnf install -q -y git gcc
+        sudo dnf install -q -y python3 python3-pip python3-setuptools python3-wheel \
+            protobuf-compiler protobuf-c-compiler protobuf-c-devel ninja-build numactl-devel czmq-devel
+    else
+        echo "Not supported OS: $SUB_OS"
+        exit 1
+    fi
+}
+
+function install_meson {
+    sudo pip3 install -Iq meson==0.57.1
+}
+
+function build_dpdk {
+    pushd dpdk
+    OPTS="-Dtests=false -Denable_kmods=false -Dmachine=default -Ddefault_library=$DEF_LIB"
+    meson build $OPTS
+    sudo ninja -C build install
+    popd
+    sudo rm -rf dpdk
+}
+
+function pkg_config {
+    export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
+}
+
+function build_xvio {
+    meson build
+    sudo ninja -C build install
+}
+
+install_package &&
+install_meson   &&
+build_dpdk      &&
+pkg_config      &&
+build_xvio
+
