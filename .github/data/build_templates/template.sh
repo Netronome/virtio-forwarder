Netronome/virtio-forwarder

commit eeafcae1bcb275cbcde16cbf8db0c3a6a80bde47

CI: set machine type to default when building DPDK
As part of building statically linked packages DPDK is also compiled
from source. Before this patch the default machine type of "native" was
used. This results in DPDK building with -march=native. This isn't very
portable and can cause issues on CPU's that don't have feature parity
with the build machine's CPU. This patch sets the machine type to
"default". On x86 this will conservatively set "-march=corei7" which is
a lot more portable than "native"

Older versions of DPDK did not export a -march flag in the pkgconfig
file. Virtio-forwarder will set the flag to core2. Change this to corei7
for consistency too.

Signed-off-by: Heinrich Kuhn <heinrich.kuhn@corigine.com>
Signed-off-by: Simon Horman <simon.horman@corigine.com>

.github/data/build_templates/template.sh
@@ -62,6 +62,7 @@ echo "Disabling the following drivers: ${disable_list%,}"
 meson -Denable_kmods=false \
       -Dmax_ethports=64 \
       -Ddisable_drivers="${disable_list%,}" \
+      -Dmachine=default \
       build
 
 ninja install -C build && ldconfig

meson.build
@@ -172,11 +172,11 @@ cflags += '-D_GNU_SOURCE'
 cflags += '-mavx'
 
 # older versions of the dpdk pc file did not include the baseline architecture
-# set it to DPDK's old default (it has since been moved to corei7 in 17.08)
-# this can be dropped once support for DPDK < 18.11 is no longer necessary
+# set it to corei7. This can be dropped once support for DPDK < 18.11
+# is no longer necessary
 dpdk_cflags = dpdk.get_pkgconfig_variable('cflags')
 if host_machine.cpu_family().startswith('x86') and not dpdk_cflags.contains('-march')
-    cflags += '-march=core2'
+    cflags += '-march=corei7'
 endif
 
 sources = files('argv.c',

commit bac3dffae114eb02cf16115d695de708ffd0f472

Add avx and sse4.2 to march flag when building statically
A previous commit changed architecture type form native to corei7. This
was done to address CPU instruction compatibility issues on older CPU's
however this change significantly impacted performance. This commit
adds AVX as an extension to corei7. AVX is added specifically to corei7
because some architectures like sandybridge and ivybridge are not known
by older GCC versions (GCC v4.8 shipped with CentOS 7 for example). SSE
is also added as it is a hard requirement for building DPDK.

Fixes: eeafcae1bcb2 ("CI: set machine type to default when building DPDK")

Signed-off-by: Heinrich Kuhn <heinrich.kuhn@corigine.com>
Signed-off-by: Simon Horman <simon.horman@corigine.com>

.github/data/build_templates/template.sh
@@ -80,10 +80,10 @@ printf -v disable_list '%s,' "${disable_drv_list[@]}"
 echo "Disabling the following drivers: ${disable_list%,}"
 
 # Build DPDK
-meson -Denable_kmods=false \
+CFLAGS="-mavx -msse -msse4.2" meson -Denable_kmods=false \
       -Dmax_ethports=64 \
       -Ddisable_drivers="${disable_list%,}" \
-      -Dmachine=default \
+      -Dmachine=corei7 \
       build
 
 ninja install -C build && ldconfig